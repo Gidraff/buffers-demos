bca2dde3f8af3d5de10d7a2c32e73783259cbb53
Gidraff <kamandegid@gmail.com>
2023-09-19 05:02:16 +0300

    feat: improve get address program

---
+++ getenvaddr.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    char *ptr;
+
+    if (argc < 3)
+    {
+        printf("Usage: %s <environment var> <target program name>", argv[0]);
+        exit(0);
+    }
+
+    ptr = getenv(argv[1]);                          /* Get env var location*/
+    ptr += (strlen(argv[0]) - strlen(argv[1])) * 2; /* Adjust for program name. */
+    printf("%s will be at %p\n", argv[1], ptr);
+}
